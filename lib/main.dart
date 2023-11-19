08cb6f03394468aa24ebb4e656891b068616699d
Sanskar Modi <modisanskar5@gmail.com>
2023-11-19 19:21:55 +0530

    Firebase Auth Settings Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weather_app/firebase_options.dart';
 import 'package:weather_app/routes/app_router.dart';
 
 Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions
+        .currentPlatform, // Initialize Firebase using provided options
+  );
   await dotenv.load(fileName: ".env");
   runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
+// final firebaseinitializerProvider = FutureProvider<FirebaseApp>((ref) async {
+//   return
+// });
+
+class MainApp extends ConsumerWidget {
   MainApp({super.key});
   final _appRouter = AppRouter();
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final initialize = ref.watch(firebaseinitializerProvider);
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: _appRouter.config(),
